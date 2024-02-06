86ae5c1d3997f8659a737a5ed848fa9640cb0a10
vishal garg <100771603+vishalgarg123@users.noreply.github.com>
2024-02-06 14:01:39 +0530

    updated

--- src/Components/movieCard/MovieCard.jsx
+++ src/Components/movieCard/MovieCard.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./style.scss";
-import Img from "../lazyLoadImage/Img";
-import {CircleRating} from "../circleRating/CircleRating";
+
+
 
 import PosterFallback from "../../assets/no-poster.png";
 
